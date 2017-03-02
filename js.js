84c234f53fda516651f6bd1aea57ab700b76d82a
olga0770 <olyalo0201@gmail.com>
2017-03-02 22:23:04 +0100

    json

--- js.js
+++ js.js
@@ -1,25 +1,24 @@
-var map;
+$.getJSON("json.json", dataErHentet);
 
-function rightClickMap() {
-map = new google.maps.event.addListener(map, "rightclick", function(event) {
-    var lat = event.latLng.lat();
-    var lng = event.latLng.lng();
-    // populate yor box/field with lat, lng
-    alert("Lat=" + lat + "; Lng=" + lng);
-});
+
+ function dataErHentet(data){
+
+ data.steder.forEach(visInformation);
+ }
+ function visInformation(sted){
+ //3. klon template så der kan indsættes data
+ var klon = document.querySelector("#informationmarker").content.cloneNode(true);
+
+ //4. hvilke data skal med?
+klon.querySelector(".data_billede").src = "picture/" + sted.billede + "-sted.jpg";
+klon.querySelector(".data_navn").textContent = sted.navn;
+klon.querySelector(".data_adresse").textContent = sted.adresse;
+klon.querySelector(".data_beskrivelse").textContent = sted.beskrivelse;
+
+
+ //5. tilføj html fra template til DOM
+ document.querySelector(".moreinfo").appendChild(klon);
 
 }
 
 
-
-
-//var map;
-//      function initMap() {
-//        map = new google.maps.Map(document.querySelector('#map'), {
-//          center: {
-//              lat: 55.706387,
-//              lng: 12.539127
-//          },
-//          zoom: 15
-//        });
-//      }
