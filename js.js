44baaff100e9d9f85a47b0b9603756c1793a6627
olga0770 <olyalo0201@gmail.com>
2017-03-04 13:07:39 +0100

    json without name

--- js.js
+++ js.js
@@ -3,17 +3,19 @@ $.getJSON("json.json", dataErHentet);
 
  function dataErHentet(data){
 
- data.steder.forEach(visInformation);
+ data.forEach(visInformation);
  }
  function visInformation(sted){
+
+console.log ("vis");
  //3. klon template så der kan indsættes data
  var klon = document.querySelector("#informationmarker").content.cloneNode(true);
 
  //4. hvilke data skal med?
 klon.querySelector(".data_billede").src = "picture/" + sted.billede + "-sted.jpg";
-klon.querySelector(".data_navn").textContent = sted.navn;
-klon.querySelector(".data_adresse").textContent = sted.adresse;
-klon.querySelector(".data_beskrivelse").textContent = sted.beskrivelse;
+klon.querySelector(".data_navn").innerHTML = sted.navn;
+klon.querySelector(".data_adresse").innerHTML = sted.adresse;
+klon.querySelector(".data_beskrivelse").innerHTML = sted.beskrivelse;
 
 
  //5. tilføj html fra template til DOM
